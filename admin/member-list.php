53f873faf443d34062a62da6bc56ae310d62766b
riyazedd <riyaz.shrestha00@gmail.com>
2024-03-06 19:38:09 +0545

    Search Feature Added in Member List

--- admin/member-list.php
+++ admin/member-list.php
@@ -6,6 +6,18 @@ $sql="SELECT * FROM members";
 $res=mysqli_query($conn,$sql);
 $sn=1;
 
+//Searching Users 
+if(!empty($_POST)){
+    $search=$_POST['search'];
+    if(!empty($search)){
+        $sql="SELECT * FROM members WHERE CONCAT(fullname,services) LIKE '%$search%'";
+        $res=mysqli_query($conn,$sql);
+    }else{
+        $sql="SELECT * FROM members";
+        $res=mysqli_query($conn,$sql);
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -15,13 +27,26 @@ $sn=1;
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>FitManage Hub - Admin</title>
         <link rel="stylesheet" href="css/member-list.css">
-        <script src="https://kit.fontawesome.com/426c1a4028.js" crossorigin="anonymous"></script>   
+        <script src="https://kit.fontawesome.com/426c1a4028.js" crossorigin="anonymous"></script>
+        <script>
+    // JavaScript function to submit form only when input field is cleared
+    function submitForm() {
+        var searchValue = document.getElementById("searchField").value.trim();
+        if (searchValue === '') {
+            document.getElementById("searchForm").submit();
+        }
+    }
+</script>  
     </head>
     <body>
     <?php include 'includes/template.php';?>
 
 <div class="content">
-    <h2>Registered Members List</h2>
+    <div class="heading"><h2>Registered Members List</h2>
+    <form action="" method="post" id="searchForm">
+        <input type="text" id="searchField" placeholder="Search" name="search" value="<?php echo isset($_POST['search']) ? htmlspecialchars($_POST['search']) : ''; ?>" oninput="submitForm()">
+        <button type="submit"><i class="fa-solid fa-search"></i></button>
+    </form></div>
     <?php if(isset($_SESSION['success'])){ ?> <div class="message"><h3><?=$_SESSION['success'];  unset($_SESSION['success'])?></h3></div><?php } ?>
     <table>
         <thead>
@@ -40,7 +65,11 @@ $sn=1;
             </tr>
         </thead>
         <tbody>
-            <?php while($user=mysqli_fetch_assoc($res)){ ?>
+            <?php if(($res->num_rows)==0){ ?>
+            <tr>
+                <td colspan="11">NO RECORD FOUND</td>
+            </tr>
+       <?php } else { while($user=mysqli_fetch_assoc($res)){ ?>
             <tr>
                 <td><?=$sn++?></td>
                 <td><?=$user['fullname']?></td>
@@ -59,7 +88,7 @@ $sn=1;
                     <a href="member-delete.php?id=<?=$user['id']?>" title="Delete Member" class="delete"><i class="fa-solid fa-trash"></i></a>
                 </td>
             </tr>
-            <?php } ?>
+            <?php }} ?>
         </tbody>
     </table>
 </div>
